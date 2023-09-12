521f1722e744df75c057c79a21fd4163a44e0367
+ <+>
2023-09-12 14:03:10 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet <= 400) {
+    return "This one is on me!"
+  } else if (distanceInFeet <= 2000) {
+    return "That will be twenty bucks."// Write your code here!
+} else if (distanceInFeet<= 2500) {
+  return "I will gladly take your thirty bucks."
+} else {
+  return "No can do."
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return (destination === "NYC") ? "Ok, sounds good." :  "No go."// Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(myTypeOfTip){
+  let tip;
+  switch (myTypeOfTip) {
+    case "generous":
+      tip = "Thank you so much."
+      break;
+    case "not as generous":
+      tip = "Thank you."
+      break;
+    default:
+      tip = "Bye."
+  }
+  return tip;
+      // Write your code here!
 }
